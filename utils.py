54af229c25e34a9f639f381cfee714336896eaac
serge <edemserge.kossi@gmail.com>
2019-03-23 21:07:19 +0000

    modification de load de SingletonModel

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ class SingletonModel(models.Model):
 	@classmethod
 	def load(cls):
 		obj, created = cls.objects.get_or_create(pk = 1)
-			return obj
+		return obj
 
 class CorrespondanceMois():
 	"""
