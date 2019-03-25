7626cdf2d3e4261afcd6c16826a2c54dc18253ce
serge <edemserge.kossi@gmail.com>
2019-03-25 15:55:26 +0000

    telephone a la place de nom dans username

--- membre/models.py
+++ membre/models.py
@@ -245,10 +245,9 @@ class EntrepriseCommerciale(Membre):
 		if self.code_membre == None:
 			super(EntrepriseCommerciale, self).save(*args, **kwargs)
 			self.code_membre = self.id
-			self.user.username=str(self.telephone) + "-" + str(self.code_membre)
-			self.user.last_name=str(self.nom)
-			self.user.password=make_password(self.mdp)
-			self.save()
+			self.user = User(username=str(self.nom) + "-" + str(self.code_membre),
+							 last_name=str(self.nom), password=make_password(self.mdp))
+			self.user.save()
 			groupe = Group.objects.get(name="Commercial")
 			groupe.user_set.add(self.user)
 			creance = Creance.objects.create(entreprise_associer=self)
