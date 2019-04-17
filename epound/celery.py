79bb002d54626660016bc9d1381fe67afd73e919
serge <edemserge.kossi@gmail.com>
2019-04-17 14:25:54 +0000

    modification de la version de django celery beat

--- epound/celery.py
+++ epound/celery.py
@@ -12,7 +12,7 @@ app = Celery('epound')
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks()
+# app.autodiscover_tasks()
 
 
 @app.task(bind=True)
