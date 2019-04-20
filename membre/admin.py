07df8b3f12454e6f987817cd69e0c2459f571f7f
serge <edemserge.kossi@gmail.com>
2019-04-20 20:08:24 +0000

    ajout du consommateur global

--- membre/admin.py
+++ membre/admin.py
@@ -130,7 +130,7 @@ class ConsommateurAdmin(PolymorphicParentModelAdmin, PolymorphicChildModelAdmin)
 	base_model = Consommateur
 	search_fields = ['nom', 'code_membre', 'actif', 'telephone']
 	child_models = (ConsommateurParticulier, ConsommateurEntreprise)
-	list_filter = (PolymorphicChildModelFilter,)s
+	list_filter = (PolymorphicChildModelFilter,)
 	list_display = ['nom_membre', 'telephone', 'code_membre', 'nationalite', 'compte_consommateur', ]
 
 	def nom_membre(self, obj):
@@ -418,6 +418,8 @@ class EntrepriseCommercialeAdmin(PolymorphicChildModelAdmin):
 	activer_membre.short_description = "Activer les Vendeur"
 
 	actions = [desactiver_membre, activer_membre]
+
+
 
 
 @admin.register(Partenaire)
