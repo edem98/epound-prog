20a70c846e9f45d7cc4aee8837b63a53ffdca298
serge <edemserge.kossi@gmail.com>
2019-04-24 16:09:19 +0000

    ajout de autoplay aux produits

--- static/js/autoplay-new-product.js
+++ static/js/autoplay-new-product.js
@@ -3,16 +3,13 @@ $(document).ready(function() {
   var owl = $("#product-slide");
 
   owl.owlCarousel({
-      items : 10, //10 items above 1000px browser width
-      itemsDesktop : [1000,5], //5 items between 1000px and 901px
-      itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      itemsTablet: [600,2], //2 items between 600 and 0
+      items : 5, //10 items above 1000px browser width
+      itemsDesktop : [1000,3], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,2], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
       itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
   });
 
-
-  $(".play").click(function(){
     owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-  })
 
 });
